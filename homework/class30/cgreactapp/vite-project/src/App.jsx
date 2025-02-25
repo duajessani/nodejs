fcabe4770f58ed8280a64f3b2a3990af1403a052
duajessani <dua.jessani@gmail.com>
2025-02-25 21:46:22 +0500

    class 30 react added

---
+++ homework/class30/cgreactapp/vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from './Component/Header/Header'
+import Footer from './Component/Footer/Footer'
+import Main from './Component/Main/Main'
+
+// import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      < Header />
+      < Main />
+      < Footer />
+     
+    </>
+  )
+}
+
+export default App
---
+++ homework/class30/cgreactapp/vite-project/src/Component/Main/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+    return(
+        <div>
+            <h1>this is main</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsum laudantium, illo sint voluptate consectetur itaque quae sunt voluptas officia.</p>
+        </div>
+    )
+}
+export default Main
